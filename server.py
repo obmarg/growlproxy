54586ddc4522c4551d705ca424a4ee7c09d4bbf7
Graeme Coupar <grambo@grambo.me.uk>
2011-12-27 23:15:58 +0000

    Initial commit.  Basic receiving of growls supported

---
+++ server.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import gntp
+import socket
+import logging
+
+class GrowlServer(object):
+    ''' Class to run a growl server '''
+    
+    connectionBacklog = 5
+
+    def __init__(self, hostname="127.0.0.1", port=23053, password=None):
+        """ 
+        Constructor for GrowlServer
+        @param: hostname    The hostname to listen on
+        @param: port        The port to listen on
+        @param: password    The password to use
+        """
+        self.run = True
+        self.socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+        self.socket.bind( ( hostname, port ) )
+        self.password = password
+
+    def Run(self):
+        """ Runs the server, listening for connections """
+        self.socket.listen( self.connectionBacklog )
+        while self.run:
+            clientSock, remoteAddress = self.socket.accept()
+            self.HandleConnection( clientSock, remoteAddress )
+            clientSock.close()
+
+    def Stop(self):
+        """ Stops the server from running """
+        self.run = False
+        if self.socket:
+            self.socket.close()
+            self.socket = None
+
+    def HandleConnection(self, clientSock, remoteAddress):
+        """
+        Handles an incoming connection
+        @param: clientSock      The socket that the client is connected to
+        @param: remoteAddress   The remote address of the client
+        """
+        message = self.Recv( clientSock )
+        if message.info[ 'messagetype' ] == 'REGISTER':
+            # Probably recieving a growl from somewhere.
+            # Send back an OK for now (maybe want to make a list of clients)
+            resp = gntp.GNTPOK( action='Register' )
+            self.Send( clientSock, 'GNTPOK', resp.encode() )
+        elif message.info[ 'messagetype' ] == 'NOTIFY':
+            # Received a growl.  Print it out to log, and respond
+            logging.info( 'Message Received: %r', message.headers[ 'Notification-Text' ] )
+            resp = gntp.GNTPOK( action='Notify' )
+            self.Send( clientSock, 'GNTPOK', resp.encode() )
+        else:
+            print "Received %s" % message.info[ 'messagetype' ]
+
+    def Send(self, socket, type, data):
+        """
+        Sends a packet on the specified socket
+        @param: socket  The socket to send the data on
+        @param: type    The type of packet to send
+        @param: data    The data to send
+        """
+        #TODO: Add logging
+        socket.send( data.encode('utf-8', 'replace') )
+
+    def Recv(self, socket):
+        """
+        Receives a packet from the specified socket
+        @param: socket  The socket to receive on
+        @returns        A gntp class representing the received data
+        """
+        message = gntp.parse_gntp( socket.recv( 1024 ), self.password )
+        if message:
+            remoteAddress = socket.getsockname()
+            logging.debug( 
+                    'From %s:%s <%s>\n%s',
+                    remoteAddress[0],
+                    remoteAddress[1],
+                    message.__class__,
+                    message 
+                    )
+        return message
+
+if __name__ == "__main__":
+    logging.basicConfig( level=logging.DEBUG )
+    gs = GrowlServer( password="password" )
+    try:
+        gs.Run()
+    except KeyboardInterrupt:
+        print "Keyboard Interupt.  Stopping"
+    finally:
+        gs.Stop()
+
