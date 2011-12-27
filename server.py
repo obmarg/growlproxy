cba50cd5ee6ccaab697edfec40b009994bedddbb
Graeme Coupar <grambo@grambo.me.uk>
2011-12-27 23:42:27 +0000

    Created GrowlClient class to wrap client sockets

--- server.py
+++ server.py
@@ -25,9 +25,10 @@ class GrowlServer(object):
         """ Runs the server, listening for connections """
         self.socket.listen( self.connectionBacklog )
         while self.run:
-            clientSock, remoteAddress = self.socket.accept()
-            self.HandleConnection( clientSock, remoteAddress )
-            clientSock.close()
+            acceptRes = self.socket.accept()
+            client = GrowlClient( *acceptRes, password=self.password )
+            self.HandleConnection( client )
+            client.Disconnect()
 
     def Stop(self):
         """ Stops the server from running """
@@ -36,45 +37,64 @@ class GrowlServer(object):
             self.socket.close()
             self.socket = None
 
-    def HandleConnection(self, clientSock, remoteAddress):
+    def HandleConnection(self, client):
         """
         Handles an incoming connection
-        @param: clientSock      The socket that the client is connected to
-        @param: remoteAddress   The remote address of the client
+        @param: client      A GrowlClient representing the client
         """
-        message = self.Recv( clientSock )
+        message = client.Recv()
         if message.info[ 'messagetype' ] == 'REGISTER':
             # Probably recieving a growl from somewhere.
             # Send back an OK for now (maybe want to make a list of clients)
             resp = gntp.GNTPOK( action='Register' )
-            self.Send( clientSock, 'GNTPOK', resp.encode() )
+            client.Send( 'GNTPOK', resp.encode() )
         elif message.info[ 'messagetype' ] == 'NOTIFY':
             # Received a growl.  Print it out to log, and respond
             logging.info( 'Message Received: %r', message.headers[ 'Notification-Text' ] )
             resp = gntp.GNTPOK( action='Notify' )
-            self.Send( clientSock, 'GNTPOK', resp.encode() )
+            client.Send( 'GNTPOK', resp.encode() )
         else:
             print "Received %s" % message.info[ 'messagetype' ]
 
-    def Send(self, socket, type, data):
+
+class GrowlClient(object):
+    """ Class to wrap a socket to a growl client """
+
+    def __init__( self, socket, remoteAddress, password ):
         """
-        Sends a packet on the specified socket
-        @param: socket  The socket to send the data on
+        Constructor
+        @param: socket          The socket the client to speak to the client on
+        @param: remoteAddress   The remote address of the client
+        @param: password        The password to use for the connection
+        """
+        self.socket = socket
+        self.remoteAddress = remoteAddress
+        self.password = password
+
+    def Disconnect(self):
+        """ Disconnects the client """
+        if self.socket:
+            self.socket.close()
+            self.socket = None
+
+    def Send(self, type, data):
+        """
+        Sends a growl packet
         @param: type    The type of packet to send
-        @param: data    The data to send
+        @param: data    The data to send (usually the result of calling 
+                        encode() on a growl object)
         """
         #TODO: Add logging
-        socket.send( data.encode('utf-8', 'replace') )
+        self.socket.send( data.encode('utf-8', 'replace') )
 
-    def Recv(self, socket):
+    def Recv(self):
         """
-        Receives a packet from the specified socket
-        @param: socket  The socket to receive on
+        Receives a growl packet
         @returns        A gntp class representing the received data
         """
-        message = gntp.parse_gntp( socket.recv( 1024 ), self.password )
+        message = gntp.parse_gntp( self.socket.recv( 1024 ), self.password )
         if message:
-            remoteAddress = socket.getsockname()
+            remoteAddress = self.socket.getsockname()
             logging.debug( 
                     'From %s:%s <%s>\n%s',
                     remoteAddress[0],
