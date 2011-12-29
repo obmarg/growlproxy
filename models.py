85c1ecfd2a2bfbe9d056a09e2ab25ce9943bc95e
Graeme Coupar <grambo@grambo.me.uk>
2011-12-29 19:21:58 +0000

    Added initial data definitions in models.py
    Initial definitions of data structures added to models.py using
    sqlalchemy ORM framework.  Not actually hooked up to much code yet, but
    a bit of testing done from python REPL.

---
+++ models.py
@@ -0,0 +1,152 @@
+'''
+This file contains the model definitions for growlproxy.
+'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship, backref
+
+engine = create_engine( 'sqlite:///database.db', echo=True )
+Base = declarative_base()
+
+GetDbSession = sessionmaker( bind = engine )
+
+class Server(Base):
+    '''
+    Class representing a remote growl server to send/receive notifications
+    from
+    '''
+
+    __tablename__ = "Servers"
+
+    id = Column( Integer, primary_key=True )
+    name = Column( String )
+    remoteHost = Column( String )
+    receiveGrowls = Column( Boolean )
+    forwardGrowls = Column( Boolean )
+    userRegistered = Column( Boolean )   
+    # True if the user registered this server.
+    # False if it was registered/subscribed through gntp
+
+    def __init__( 
+            self,
+            name,
+            remoteHost,
+            receiveGrowls=False,
+            forwardGrowls=False,
+            userRegistered=False
+            ):
+        '''
+        Constructor.
+        '''
+        self.name = name
+        self.remoteHost = remoteHost
+        self.receiveGrowls = receiveGrowls
+        self.forwardGrowls = forwardGrowls
+        self.userRegistered = userRegistered
+
+
+class ServerGroup(Base):
+    '''
+    Class representing a group of servers.
+    Each server should have it's own server group, and new ones can be created
+    when several servers need to be in the same group
+    '''
+
+    __tablename__ = "ServerGroups"
+
+    id = Column( Integer, primary_key=True )
+    name = Column( String )
+    members = relationship( 'ServerGroupMembership', backref='group' )
+
+
+class ServerGroupMembership(Base):
+    '''
+    Model to hold the membership of a server group
+    '''
+
+    __tablename__ = "ServerGroupMemberships"
+
+    groupId = Column( 
+            Integer,
+            ForeignKey( 'ServerGroups.id' ),
+            primary_key = True
+            )
+    serverId = Column( 
+            Integer,
+            ForeignKey( 'Servers.id' ),
+            primary_key = True
+            )
+    priorirty = Column( Integer )
+    # Ascending list.  If all 0, all equal priority
+
+    #TODO: Possibly add an AssociationProxy somewhere to simplify things.
+    #       At least for the server -> group relationship anyway.
+    #       Probably less neccesary for the reverse`
+    server = relationship(
+            'Server',
+            backref = 'groupMemberships'
+            )
+
+
+class ForwardRule(Base):
+    '''
+    Class representing a growl forward rule
+    '''
+
+    __tablename__ = "ForwardRules"
+
+    id = Column( Integer, primary_key=True )
+    name = Column( String )
+    fromServerGroupId = Column( Integer, ForeignKey( 'ServerGroups.id' ) )
+    toServerGroupId = Column( Integer, ForeignKey( 'ServerGroups.id' ) )
+    sendToAll = Column( Boolean )           
+    # Consider forwarding done if one server is online
+    # Otherwise, only send to highest priority server
+    # (Or random choice if all same priority)
+    storeIfOffline = True       
+    # Store the growl for later if all servers offline
+
+    # TODO: Find out how to make the below link straight to a list of servers?
+    #       Also, maybe add a backref
+    #fromServerGroup = relationship( "ServerGroup" )
+    #toServerGroup = relationship( "ServerGroup" )
+    # TODO: Bet the above doesn't work because there's 2 seperate things.
+    #       Figure out how to fix it and then uncomment
+
+    def __init__(
+            self,
+            name,
+            fromServerGroupId,
+            toServerGroupId,
+            sendToAll=False,
+            storeIfOffline=True
+            ):
+        """ Constructor """
+        self.name = name
+        self.fromServerGroupId = fromServerGroupId
+        self.toServerGroupId = toServerGroupId
+        self.sendToAll = sendToAll
+        self.storeIfOffline = storeIfOffline
+
+
+class RuleFilter(Base):
+    '''
+    Class representing a filter for forward rules
+    Fields in this table should be ANDed together
+    Rows in this table should be ORed together
+    '''
+
+    __tablename__ = "RuleFilters"
+
+    id = Column( Integer, primary_key=True )
+    ruleId = 0 # TODO: Foreign key me
+    applicationName = Column( String )
+    growlTitle = Column( String )
+    growContentsRegex = Column( String )  # Regular expression of growl contents
+
+    #TODO: Define a constructor for the above, with default "" for all 
+    #      filter criteria
+
+
