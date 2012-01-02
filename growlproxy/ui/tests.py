4ea5b2532691546274ddce93fef950a8833f5513
Graeme Coupar <grambo@grambo.me.uk>
2012-01-02 22:03:39 +0000

    Added server list with delete functionality to UI
    - Added server list to the UI with add & delete buttons and selectable
      list
    - Add button currently does nothing
    - Delete button should delete all selected servers
    - Added generic "Confirm Delete" dialog to base.html enabled based on
      confirmDeleteDialog template variable
    - Added basic unit tests of the server list and delete JSON API

--- growlproxy/ui/tests.py
+++ growlproxy/ui/tests.py
@@ -28,3 +28,71 @@ class IndexTestCase(UiTestCase):
         assert 'Manage Groups' in rv.data
         assert 'Manage Forwarding Rules' in rv.data
         assert 'Manage Configuration' in rv.data
+
+class ServerListTestCase(UiTestCase):
+    def setUp(self):
+        ''' Creates a bunch of servers to use in the tests '''
+        super( ServerListTestCase, self ).setUp()
+        self.db = growlproxy.db.GetDbSession()
+        self.db.add( models.Server( 
+            name='There is no place like it',
+            remoteHost='127.0.0.1',
+            receiveGrowls = True,
+            forwardGrowls = True,
+            userRegistered = True
+            ) )
+        self.db.add( models.Server(
+            name= 'Testing 2',
+            remoteHost = '192.168.5.1'
+            ) )
+        self.db.commit()
+
+    def tearDown(self):
+        self.db.close()
+        self.db = None
+        super( ServerListTestCase, self ).tearDown()
+
+    def test_ServerList(self):
+        rv = self.app.get('/servers/')
+        #TODO: verify the xml properly?
+        assert 'There is no place like it' in rv.data
+        assert 'Testing 2' in rv.data
+
+    def test_DeleteNoServer(self):
+        # Test sending no id
+        rv = self.app.post('/api/DeleteServer/' )
+        self.assertIn( '500', rv.status ) 
+
+    def test_DeleteGarbageServer(self):
+        # Test sending garbage server ids
+        rv = self.app.post(
+                '/api/DeleteServer/',
+                data = { 'id' : 'fuckingbullshit' }
+                )
+        self.assertIn( '500', rv.status ) 
+
+    def test_DeleteOneServer(self):
+        id = self.db.query( models.Server ).first().id
+        rv = self.app.post(
+                '/api/DeleteServer/',
+                data = { 'id' : json.dumps( [ id ] ) }
+                )
+        self.assertIn( '200', rv.status )
+        self.assertEqual(
+                self.db.query( models.Server ).count(),
+                1
+                )
+
+    def test_DeleteAllServers(self):
+        ids = [ entry[0] for entry in self.db.query( models.Server.id ).all() ]
+        rv = self.app.post(
+                '/api/DeleteServer/',
+                data = { 'id' : json.dumps( ids ) }
+                )
+        self.assertIn( '200', rv.status )
+        self.assertEqual( 
+                self.db.query( models.Server ).count(),
+                0
+                )
+
+
--- growlproxy/ui/views.py
+++ growlproxy/ui/views.py
@@ -1,6 +1,9 @@
-from flask import request, session, g, redirect, url_for, render_template
+
+import json
+from flask import request, session, g, redirect, url_for, render_template, abort
 from growlproxy.ui import app, LoadDb
 from growlproxy import models
+
 
 @app.before_request
 def BeforeRequest():
@@ -16,11 +19,40 @@ def TeardownRequest( exception ):
     @param: exception   The exception that occurred (if any)
     """
     if exception:
+        print "Rolling back"
         g.db.rollback()
     else:
+        g.db.commit()
         g.db.close()
     g.db = None
 
 @app.route('/')
-def index():
+def Index():
     return render_template( 'index.html' )
+
+@app.route('/servers/')
+def ServerList():
+    options = {
+            'servers' : g.db.query( models.Server ),
+            'confirmDeleteDialog' : { 
+                'itemType' : 'Server',
+                'url' : url_for( 'DeleteServer' )
+                }
+            }
+    return render_template(
+            'serverList.html',
+            **options
+            )
+
+@app.route('/api/DeleteServer/', methods=['POST'])
+def DeleteServer():
+    if 'id' not in request.form:
+        abort(500)
+    try:
+        ids = json.loads( request.form['id'] )
+    except:
+        abort(500)
+    g.db.query( models.Server ).filter(
+            models.Server.id.in_( ids ) 
+            ).delete( False )
+    return "OK"
